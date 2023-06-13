3d156359b39cb134cf148278d5fde300eb2cbefa
TanBryant <nhattan0932507137@gmail.com>
2023-06-13 11:14:22 +0700

    v4

--- src/App.js
+++ src/App.js
@@ -1,27 +1,75 @@
 import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 
 
 function App() {
+  const [work, setWork] = useState("");
+  const [todos, setTodos] = useState([])
+  const handleClickAdd = () => {
+    if (todos?.some((item) => item.id === work?.replace(/\s/g, ""))) {
+      toast.warn('Công việc đã được thêm trước đó')
+    } else {
+      setTodos((prev) => [
+        ...prev,
+        { id: work?.replace(/\s/g, ""), job: work },
+      ]);
+      setWork('');
+    }
+  };
+  const hanldeDeleteJob = (id) => {
+    setTodos(prev => prev.filter(item => item.id !== id))
+  }
 
-  const [work, setWork] = useState('')
-
+  console.log(todos)
   return (
-      <h1 className="flex gap-8 h-screen justify-center border border-red-500 items-center">
-        <input 
-          type="text" 
-          className="outline-none border border-blue-600 px-4 py-2 w-[300px]"
-          value={work}
-          onChange={e => setWork(e.target.value)}
-        />
+    <>
+      <div className="flex flex-col gap-8 h-screen justify-center border border-red-500 items-center">
+        <div className="flex gap-8">
+          <input
+            type="text"
+            className="outline-none border border-blue-600 px-4 py-2 w-[300px]"
+            value={work}
+            onChange={(e) => setWork(e.target.value)}
+          />
 
-        <button
-          type="button"
-          className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
-        >
-          Add
-        </button>
-      </h1>
+          <button
+            type="button"
+            className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
+            onClick={handleClickAdd}
+          >
+            Add
+          </button>
+        </div>
+        <div>
+          <h3 className="font-bold text-xl">Content :</h3>
+          <ul>
+            {todos?.map((item) => {
+              return (
+                <li key={item.id} className="flex gap-10 items-center ">
+                  <span className="my-2 ">{item.job}</span>
+                  <span onClick={() => hanldeDeleteJob(item.id)} className="my-2 cursor-pointer p-2"> X </span>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
   );
 }
 
